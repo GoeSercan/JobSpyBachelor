5bdbab9274747434a8a8d60c7a948b48b9a6c751
GoeSercan <sercan.goenuelcan@hotmail.com>
2024-12-05 05:16:45 +0100

    Update config for proxy and SSL certificate setup

--- src/config.py
+++ src/config.py
@@ -11,6 +11,19 @@ BRD_PASSWD = os.getenv("BRD_PASSWD")
 BRD_SUPERPROXY = os.getenv("BRD_SUPERPROXY")
 CA_CERT_PATH = os.getenv("CA_CERT_PATH")
 
+# File paths
+OFFSET_FILE = "offsets.json"
+DAILY_JOB_COUNT_FILE = "daily_job_counts.json"
+LOG_FILE = "scraping_log.txt"
+
+# Parameters for scraper
+RESULTS_PER_PAGE_OPTIONS = [40]
+NO_RESULT_THRESHOLD = 2
+
+# Parameters for runner
+MAX_RUNS = 576
+INTERVAL_SECONDS = 150
+
 # Load JSON files
 with open("countries.json") as f:
     COUNTRIES = json.load(f)
